7e38f978a569f460dccc5aad4d33cb6a8bd060ac
Mario Reder <mario.reder@pm.me>
2025-10-31 06:52:57 +0100

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -25,7 +25,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_TWITTER_URL": "",
   "VITE_SEO_SITE_NAME": "Cypherpunk",
   "VITE_SEO_SITE_DESCRIPTION": "the encrypted dex on solana",
-  "VITE_SEO_SITE_URL": "https://dex.orderly.network/superdex-3501/",
+  "VITE_SEO_SITE_URL": "https://cypherdex.org",
   "VITE_SEO_SITE_LANGUAGE": "en",
   "VITE_SEO_SITE_LOCALE": "en_US",
   "VITE_SEO_TWITTER_HANDLE": "",
